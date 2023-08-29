3167dc2074b51c1e3cc55c3773f9e69a38f2560f
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-08-29 09:19:14 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 이예빈 on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
