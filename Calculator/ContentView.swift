abe941a5611dcdcf516b118d7c9f59db962d482b
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-08-31 17:15:50 +0900

    refactor : enum 활용하기

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,16 +7,86 @@
 
 import SwiftUI
 
+enum ButtonType: String {
+    case first, second, third, fourth,fifth, sixth, seventh, eighth, nineth, zero
+    case dot, equal, add, subtract, multiply, divide
+    case percent, opposite, clear
+    
+    var ButtonDisplayName: String {
+        switch self {
+        case .first :
+            return "1"
+        case .second :
+            return "2"
+        case .third :
+            return "3"
+        case .fourth :
+            return "4"
+        case .fifth :
+            return "5"
+        case .sixth :
+            return "6"
+        case .seventh :
+            return "7"
+        case .eighth :
+            return "8"
+        case .nineth :
+            return "9"
+        case .zero :
+            return "0"
+        case .dot :
+            return "."
+        case .equal :
+            return "="
+        case .add :
+            return "+"
+        case .subtract :
+            return "-"
+        case .multiply :
+            return "X"
+        case .divide :
+            return "$"
+        case .percent :
+            return "%"
+        case .clear :
+            return "C"
+        case .opposite :
+            return "/"
+            
+        }
+    }
+    
+    var backgroundColor: Color{
+        switch self {
+        case .first, .second,.third, .fourth, .fifth, .sixth, .seventh, .eighth, .nineth, .zero, .dot:
+            return Color("NumberButton")
+        case .equal, .add, .subtract, .multiply, .divide:
+            return Color(.orange)
+        case .percent, .opposite, .clear:
+            return Color("SymbolButton")
+        }
+    }
+    
+    var foregroundColor : Color {
+        switch self {
+        case .first, .second,.third, .fourth, .fifth, .sixth, .seventh, .eighth, .nineth, .zero, .dot, .equal, .add, .subtract, .multiply, .divide:
+            return Color(.white)
+        case .percent, .opposite, .clear:
+            return Color(.black)
+        }
+    }
+}
+
 struct ContentView: View {
     
     @State private var totalNumber: String = "0"
     
-    private let buttonData: [[String]] = [
-        ["C","/","%","$"],
-        ["7","8","9","X"],
-        ["4","5","6","-"],
-        ["1","2","3","+"],
-        ["0","0",".","="],
+    private let buttonData: [[ButtonType]] = [
+        [.clear,.opposite,.percent,.divide],
+        [.seventh, .eighth, .nineth, .multiply],
+        [.fourth, .fifth, .sixth, .subtract],
+        [.first, .second, .third, .add],
+        [.zero, .dot, .equal],
     ]
     
     var body: some View {
@@ -44,16 +114,16 @@ struct ContentView: View {
                             
                             Button{
                                 if totalNumber == "0" {
-                                    totalNumber = row
+                                    totalNumber = row.ButtonDisplayName
                                 } else {
-                                    totalNumber += row
+                                    totalNumber += row.ButtonDisplayName
                                 }
                             } label: {
-                                Text(row)
+                                Text(row.ButtonDisplayName)
                                     .frame(width: 80, height:80)
-                                    .background(Color("NumberButton"))
+                                    .background(row.backgroundColor)
                                     .cornerRadius(40)
-                                    .foregroundColor(.white)
+                                    .foregroundColor(row.foregroundColor)
                                     .font(.system(size: 33))
                             }
                             
